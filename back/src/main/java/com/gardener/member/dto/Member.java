90be9ff487e96257edb2be82390f3d34c1b05e1f
LMH <towblue@naver.com>
2023-08-01 19:04:26 +0900

    수정된 파일

--- back/src/main/java/com/gardener/member/dto/Member.java
+++ back/src/main/java/com/gardener/member/dto/Member.java
@@ -1,19 +1,6 @@
 package com.gardener.member.dto;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 public class Member {
-   
-=======
-public class Member {
 
->>>>>>> origin
-=======
-=======
-
->>>>>>> ca6ce0378bd2e378480644d4496b70b73bf43b04
-public class Member {
-
->>>>>>> 0d53565f9944c2b41e90e1f5d3023e3a9414b2b7
 }
