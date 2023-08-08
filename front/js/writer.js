d0e5b3fa01a4726dda267be629f041c692a6fe85
sunjin <jungsunjin9@gmail.com>
2023-08-08 16:14:46 +0900

    .

--- front/js/writer.js
+++ front/js/writer.js
@@ -1,12 +1,32 @@
 $(() => {
-  //header를 넣을 태그 찾기
-  const header = $('body>div.header');
-  //div.header class속성에 header.html불러오기
-  header.load('../html/header.html');
+  //header
+  const header = $("body>div.header");
+  header.load("../html/header.html");
 
-  //footer를 넣을 태그 찾기
-  const footer = $('body>div.footer');
-  //div.header class속성에 header.html불러오기
-  footer.load('../html/footer.html');
+  //footer
+  const footer = $("body>div.footer");
+  footer.load("../html/footer.html");
 
-}
+  //글내용 보여주기
+  $.ajax({
+    url: "http://localhost:8888/back/writerpost", //실제서버주소
+    type: "get",
+    data: { writerid: 1 },
+    success: (response) => {
+      console.log(response);
+      // if (response[1] === null) {
+      //   alert("글이 없습니다");
+      //   $("#post-title-writer").hide();
+      //   $("#post-content").hide();
+      //   $("#thumbnail").hide();
+      // } else {
+      //   alert("연결완료");
+      //   $("#post-title-writer").text(response[1].mainTitle);
+      //   $("#post-content").text(response[1].content);
+      // }
+    },
+    error: () => {
+      alert("에러발생");
+    },
+  });
+});
