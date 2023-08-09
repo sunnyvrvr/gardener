5820b0e6915cbd6786c0c68601700fab8f5e2107
LMH <towblue@naver.com>
2023-08-09 17:41:48 +0900

    머지후 커밋

--- back/src/main/java/com/gardener/search/controller/SearchController.java
+++ back/src/main/java/com/gardener/search/controller/SearchController.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
